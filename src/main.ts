5912abf3e592b85306156061a12b1ed4bafb9fbd
ACharLuk <acharluk@gmail.com>
2018-02-05 21:59:11 +0100

    Test 0.0.3

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { LunaManager } from './luna'
 import * as extract_zip from 'extract-zip'
 
-console.log(process.argv)
-
 switch(process.argv[2]) {
     case '--update':
         LunaManager.checkForUpdates()
